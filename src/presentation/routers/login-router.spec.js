11ebe39ff9849f806427c1918839a5958839db69
Luan Eli Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-04-16 16:50:47 -0300

    chore: move sut creation to a factory helper method

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,13 @@
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
+const makeSut = () => {
+  return new LoginRouter()
+}
+
 describe('Login Router', () => {
   test('should return 400 if no email is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -17,7 +21,7 @@ describe('Login Router', () => {
   })
 
   test('should return 400 if no password is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com'
@@ -31,7 +35,7 @@ describe('Login Router', () => {
   })
 
   test('should return 500 if no httpRequest is provided', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
 
     const httpResponse = sut.route()
 
@@ -39,7 +43,7 @@ describe('Login Router', () => {
   })
 
   test('should return 500 if no httpRequest has no body', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {}
 
     const httpResponse = sut.route(httpRequest)
@@ -48,7 +52,7 @@ describe('Login Router', () => {
   })
 
   test('should return 200 if succesfully', () => {
-    const sut = new LoginRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
