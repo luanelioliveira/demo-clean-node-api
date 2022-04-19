882293dd0c37ff26cc6a566c81eb3499ef1f68af
Luan Eli Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-04-19 19:51:29 -0300

    chore: ensure AuthUseCase throws if no email provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,17 @@
+class AuthUseCase {
+  async auth (email) {
+    if (!email) {
+      throw new Error()
+    }
+  }
+}
+
+describe('Auth UseCase', () => {
+  test('should return null if email no provided', async () => {
+    const sut = new AuthUseCase()
+
+    const promise = sut.auth()
+
+    expect(promise).rejects.toThrow()
+  })
+})
