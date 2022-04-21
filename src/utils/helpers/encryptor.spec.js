362824d4ac536be5bd27d68d872f7eef4e0d01b4
Luan Eli Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-04-21 12:56:07 -0300

    chore: ensure Encrypter returns false if descrypt returns false

--- src/utils/helpers/encryptor.spec.js
+++ src/utils/helpers/encryptor.spec.js
@@ -1,6 +1,8 @@
+const bcrypt = require('bcrypt')
 class Encrypter {
-  async compare (password, hashedPassword) {
-    return true
+  async compare (data, hash) {
+    const isValid = bcrypt.compare(data, hash)
+    return isValid
   }
 }
 
@@ -8,8 +10,17 @@ describe('Encrypter', () => {
   test('should return true if bcrypt returns true', async () => {
     const sut = new Encrypter()
 
-    const isValid = await sut.compare('any_password', 'hashed_password')
+    const isValid = await sut.compare('any_data', 'any_hash')
 
     expect(isValid).toBe(true)
+  })
+
+  test('should return false if bcrypt returns false', async () => {
+    const sut = new Encrypter()
+    bcrypt.isValid = false
+
+    const isValid = await sut.compare('any_data', 'any_hash')
+
+    expect(isValid).toBe(false)
   })
 })
