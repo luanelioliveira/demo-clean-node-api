ffd5497e461cd5aceb97e6d82627b885230e6688
Luan Eli Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-04-22 10:03:11 -0300

    chore: move TokenGenerator to own file

---
+++ src/utils/helpers/token-generator.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const MissingParamError = require('../errors/missing-param-error')
+
+module.exports = class TokenGenerator {
+  constructor (secret) {
+    this.secret = secret
+  }
+
+  async generate (data) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
+
+    if (!data) {
+      throw new MissingParamError('data')
+    }
+
+    return jwt.sign(data, this.secret)
+  }
+}
--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,23 +1,6 @@
 const jwt = require('jsonwebtoken')
 const MissingParamError = require('../errors/missing-param-error')
-
-class TokenGenerator {
-  constructor (secret) {
-    this.secret = secret
-  }
-
-  async generate (data) {
-    if (!this.secret) {
-      throw new MissingParamError('secret')
-    }
-
-    if (!data) {
-      throw new MissingParamError('data')
-    }
-
-    return jwt.sign(data, this.secret)
-  }
-}
+const TokenGenerator = require('./token-generator')
 
 const makeSut = () => {
   const sut = new TokenGenerator('any_secret')
